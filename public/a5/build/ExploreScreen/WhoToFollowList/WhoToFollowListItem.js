8ccb40976f7fb0046757c26e8d5eaf762a68391c
priyalpat721 <priyalpat721@gmail.com>
2021-10-14 19:40:08 -0400

    build for A5

--- public/a5/build/ExploreScreen/WhoToFollowList/WhoToFollowListItem.js
+++ public/a5/build/ExploreScreen/WhoToFollowList/WhoToFollowListItem.js
@@ -2,14 +2,14 @@ const WhoToFollowListItem = (who) => {
     return `<li class="list-group-item">
         <div class="row">
             <div class="col-2">
-            <img src="${who.avatarIcon}" class="rounded-circle w-100 h-100"/></div>
-            <div class="col-xxl-7 col-lg-6 text-nowrap">
+            <img src="${who.avatarIcon}" class="rounded-circle wd-thumbnails1"/></div>
+            <div class="col-xxl-6 col-lg-6 text-nowrap ms-1">
                 <label>${who.userName}</label>
                 <i class="fas fa-check-circle fa-xs"></i>
             </br>
             <label>${who.handle}</label>
             </div>
-            <div class="col-xxl-3 col-lg-4 ms-0">
+            <div class="col-xxl-3 col-lg-3 ms-0">
                 <button class="btn btn-primary override-bs1 align-content-between">Follow</button>
             </div>
         </div>
