8a8c6ed0f2324a6794c07abea44786cb0386fc0f
priyalpat721 <priyalpat721@gmail.com>
2021-11-19 21:03:28 -0500

    heroku added

--- src/App.js
+++ src/App.js
@@ -12,11 +12,6 @@ import {combineReducers, createStore} from "redux";
 import {Provider} from "react-redux";
 import React from "react";
 
-import A9 from "./a9";
-import Practice9 from "./a9/Practice";
-import A6 from "./components/a6";
-import A7 from "./components/a7";
-import A8 from "./components/a8";
 
 const reducer = combineReducers({tweets: tweetsReducer, who: whoReducer, profile: profileReducer})
 const store = createStore(reducer);
@@ -31,24 +26,7 @@ function App() {
                 <Link to="/a8/">A8</Link> |
                 <Link to="/a9/">A9</Link>
 
-                <div className="container">
-                    <Route path={["/", "/a6"]} exact={true}>
-                        <A6/>
-                    </Route>
-                    <Route path={["/", "/a7"]} exact={true}>
-                        <A7/>
-                    </Route>
-                    <Route path={["/", "/a8"]} exact={true}>
-                        <A8/>
-                    </Route>
-                    <Route path={["/", "/a9", "/a9/practice"]} exact={true}>
-                        <Practice9/>
-                    </Route>
-                    <Route path="/a9">
-                        <A9/>
-                    </Route>
 
-                  </div>
             </BrowserRouter>
         </Provider>
     );
